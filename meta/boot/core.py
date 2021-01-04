2a0e7c933f3235fa569f08d9f2f1f1c52e34fae8
Vili Vilenius <vili.vilenius@outlook.com>
2021-01-04 20:53:24 +0200

    Logo styling

--- meta/boot/core.py
+++ meta/boot/core.py
@@ -1,15 +1,15 @@
 from termcolor import colored
 
 logo = """
-         AWAMMMMM  MMMMMMMMMMMM  MM       MM  MMMMMMM  MMMMA
-        AWMM            MM       MM       MM  MM       MM   RA
-       AW MM            MM       MM       MM  MM       MM    RD
-      AW  MM            MM       MM       MM  MM       MM   RW
-     AW   MMMMMMM       MM       MMMMMMMMMMM  MMMMMMM  MMWMW
-    AW    MM            MM       MM       MM  MM       MM WA
-   AWMMMMMMM            MM       MM       MM  MM       MM  WA
-  AW      MM            MM       MM       MM  MM       MM   WA
- AW       MMMMMMM       MM       MM       MM  MMMMMMM  MM    WA"""
+         AWAMMMMM  MMMMMMMMMMMM  MM      MM  MMMMMMMM  MMMMMA
+        AWMM            MM       MM      MM  MM        MM    RA
+       AW MM            MM       MM      MM  MM        MM     RD
+      AW  MM            MM       MM      MM  MM        MM    RW
+     AW   MMMMMMM       MM       MMMMMMMMMM  MMMMMMMM  MMWMMW
+    AW    MM            MM       MM      MM  MM        MM WA
+   AWMMMMMMM            MM       MM      MM  MM        MM  WA
+  AW      MM            MM       MM      MM  MM        MM   WA
+ AW       MMMMMMM       MM       MM      MM  MMMMMMMM  MM    WA"""
 intro = """
 Hello there!
 
