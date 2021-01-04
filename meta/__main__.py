d8c8706fa669f751aa5a103c1f56d3145ca247e4
Vili Vilenius <vili.vilenius@outlook.com>
2021-01-04 14:46:19 +0200

    Naming more consistent

--- meta/__main__.py
+++ meta/__main__.py
@@ -2,20 +2,15 @@
 # -*- coding: utf-8 -*-
 import sys
 import yaml
-from . import API
+from . import API as meta
 #───────────────────────────────────────────────────────────────────────
 if "dump" in sys.argv:
-    API.contructor.dump_boot(API.path_0)
+    meta.contructor.generate_boot(meta.path_0)
 
 if "load" in sys.argv:
-    with open(API.path_0, 'r+') as file:
+    with open(meta.path_0, 'r+') as file:
         item = yaml.load(file, Loader=yaml.Loader)
 
-if "launchDB" in sys.argv:
-    print("derp")
-    from .. import database
-    
-
 if "print" in sys.argv:
-    with open(API.path_DB/"__main__.py", 'r+') as file:
+    with open(meta.path_DB/"__main__.py", 'r+') as file:
         print(file.read())
