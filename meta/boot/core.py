ec4ce33714324c76dea1c7bab47d5702ddc6939e
Vili Vilenius <vili.vilenius@outlook.com>
2021-01-04 14:45:36 +0200

    Added some more output

--- meta/boot/core.py
+++ meta/boot/core.py
@@ -5,3 +5,16 @@ import yaml
 path_DB = pathlib.Path(__file__).parent.absolute()
 print(path_DB)
 print(("Metaprogramming", argv[1]))
+
+intro = """
+Hello there!
+The database has been booted, but it is not yet functional."""
+print(intro)
+
+inputprompt = """
+To show some interactivity from the database,
+please provide something to echo:  """
+
+response = input(inputprompt)
+print("Echo: %s" % str(response))
+
